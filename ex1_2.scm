86e132d4c882c11f8b4f9a0fac96851efbdc927c
Reverse-Polish <77e5300e@opayq.com>
2017-07-03 21:29:03 +0200

    Update ex1_2.scm

--- ex1_2.scm
+++ ex1_2.scm
@@ -1,29 +1,34 @@
-; The main structure (super structure - main node) consists of a primitive procedure in the form of division and two combinations represented by the numerator and denominator (two of the three main branches and nodes if the structure is represented as a tree).
+; The main structure (super structure - main node) consists of a primitive procedure in the form of division and
+; two combinations represented by the numerator and denominator (two of the three main branches and node structure
+; is represented as a tree).
 
 
 ; Super structure - main node:
--> ((primitive procedure) (combination)(combination))
--> (/ (num) (denom))
+; -> ((primitive procedure) (combination)(combination))
+; -> (/ (num) (denom))
 
 ; Num:
-The numerator is a nested structure with five sub levels of combinations. The innermost combinationis the rational number 4/5, translated to (/ 4 5) in prefix notation. From this core, the return value of the combinations percolate upwards ending as node in the combination (+ 5 4 <value of nested combinations>).
+; The numerator is a nested structure with five sub levels of combinations. The innermost combinationis the rational 
+; number 4/5, translated to (/ 4 5) in prefix notation. From this core, the return value of the combinations percolate
+; upwards ending as node in the combination (+ 5 4 <value of nested combinations>).
 
-Fully substituted the numerator looks like the following:
+; Fully substituted the numerator looks like the following:
 
--> (+ 5 4 (- 2 (-3 (+ 6 (/ 4 5)))))
+; -> (+ 5 4 (- 2 (-3 (+ 6 (/ 4 5)))))
 
 ; Denom:
-The denominator is a combination combining a primitive expression and two expressions by multiplication. The two sub combinations are combines primitive expressions by subtraction:
+; The denominator is a combination combining a primitive expression and two expressions by multiplication. The two sub
+; combinations are combines primitive expressions by subtraction:
 
-Fully substituted the denominator looks like the following:
+; Fully substituted the denominator looks like the following:
 
--> (* 3 (- 6 2)(- 2 7))
+; -> (* 3 (- 6 2)(- 2 7))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; The whole expression translated into prefix notaation:
+; The whole expression translated into prefix notation:
 
 (/ (+ 5 4 (- 2 (-3 (+ 6 (/ 4 5))))) (* 3 (- 6 2)(- 2 7)))
 
@@ -38,9 +43,6 @@ Fully substituted the denominator looks like the following:
  (* 3 
     (- 6 2)
     (- 2 7)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
-
